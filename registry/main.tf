1fb35ba6ec4b1740247cd2a17b7ea0b273290eae
NawfelHilal <nawfelhilal13@gmail.com>
2026-02-06 09:00:26 +0100

    fermeture port 5000

--- registry/main.tf
+++ registry/main.tf
@@ -94,13 +94,6 @@ resource "aws_security_group" "registry_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "Registry Docker API"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 0
     to_port     = 0
